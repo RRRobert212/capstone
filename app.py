6254e9720a1062c6c7b4513304e80d8ce24994f6
RRRobert212 <robertwd1@gmail.com>
2025-04-12 01:50:44 -0400

    just for testing rn

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import os
+from poker_analysis import parser, stats, plots
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    charts_html = []
+
+    if request.method == "POST":
+        file = request.files.get("file")
+        if file and file.filename.endswith(".csv"):
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+
+            df = parser.load_log(filepath)
+            player_dict = parser.create_player_dict(df)
+
+            #player presence, number of hands
+            hands = stats.track_player_presence(df, player_dict)
+            print("Hands: ", hands)
+            callcount = stats.track_preflop_calls(df, player_dict)
+            print("CALL COUNTS: ", callcount)
+            pfr = stats.track_preflop_raises(df, player_dict)
+            print("PFR: ", pfr)
+            noflopraise = stats.track_pfr_noflop(df, player_dict)
+            print('PFRNOFLOP: ', noflopraise)
+            noflopcalls = stats.track_calls_noflop(df, player_dict)
+            print('CALLNOFLOP: ', noflopcalls)
+
+            # Collect stats for calls, raises, bets, and aggression factor
+            calls = stats.get_action_counts(df, 'calls', player_dict)
+            raises = stats.get_action_counts(df, 'raises', player_dict)
+            bets = stats.get_action_counts(df, 'bets', player_dict)
+            folds = stats.get_action_counts(df, 'folds', player_dict, 3)
+
+            aggression = stats.calc_aggression_factor(bets, raises, calls)
+
+            # Generate interactive charts for each stat
+            charts_html.append(plots.plot_bar_chart(player_dict, aggression, "Aggression Factor", "Player", "AF"))
+            charts_html.append(plots.plot_bar_chart(player_dict, calls, "Calls", "Player", "Number of Calls"))
+            charts_html.append(plots.plot_bar_chart(player_dict, raises, "Raises", "Player", "Number of Raises"))
+            charts_html.append(plots.plot_bar_chart(player_dict, bets, "Bets", "Player", "Number of Bets"))
+            charts_html.append(plots.plot_bar_chart(player_dict, folds, "Folds", "Player", "Number of Folds"))
+
+    return render_template("index.html", charts=charts_html)
+
+if __name__ == "__main__":
+    app.run(debug=True)
